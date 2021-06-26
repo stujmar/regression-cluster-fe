cf6d67b029b460cfc5c21a7719a23da333cd8f64
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-25 19:01:45 -0500

    [sm] got use memo to work and grid updates without any errors

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -4,14 +4,18 @@ import {title} from './Grid.module.css'
 
 const Grid = () => {
     const [ gridData, setGridData ] = useState([])
-    const rangeX = ["1x","2x","3x","4x"];
-    const rangeY = ["1y","2y","3y","4y"];
+
+    const rangeX = useMemo(() => {
+        return ["1x","2x","3x","4x"];
+    },[])
+    
+    // const rangeY = ["1y","2y","3y","4y"];
 
     useEffect(() => {
         setGridData(rangeX.map((cell) => {
             return <Cell key={cell} />
         }))
-    },[rangeX, rangeY])
+    },[rangeX])
 
     return(<div className={`${title}`}>This is the Grid.{gridData}</div>)
 }
