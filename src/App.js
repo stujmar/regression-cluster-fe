6820fcfbbdfbeb702c1640aab2c412986d9ff0aa
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-26 11:15:10 -0500

    [sm] got onmousemove and mouse max x and y working but not right percentages yet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
+import React, {useState} from 'react'
 import FlexGrid from './components/FlexGrid/FlexGrid';
 
 function App() {
+
+  // const [dots, setDots] = useState([]);
+  const [ mouse, setMouse] = useState({ x: 0, y: 0, xMax: 0, yMax: 0});
+  let grid = document.getElementById('grid');
+
+  const handleMouseMove = (e) => {
+    console.log(e);
+    setMouse({
+      x:e.nativeEvent.offsetX > 0 ? e.nativeEvent.offsetX : 0,
+      y:e.nativeEvent.offsetY > 0 ? e.nativeEvent.offsetY : 0,
+      xMax: grid.clientWidth,
+      yMax: grid.clientWidth
+  })
+  // console.log(mouse)
+  }
+
   return (
     <div className="label-wrapper">
       <div className="x-zero label">0</div>
       <div className="y-zero label">0</div>
       <div className="x-100 label">100</div>
       <div className="y-100 label">100</div>
-      <button className="clean-button" onClick={(() => {console.log("grid clicked")})}>
+      <button 
+        id="grid"
+        className="clean-button"
+        onMouseMove={(e) => handleMouseMove(e)}
+        onClick={(() => {console.log("grid clicked")})}>
         <FlexGrid />
       </button>
     </div>
