8c86d4de33f3128a0df609f787c5ecf1cd67d3dc
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-26 09:52:49 -0500

    [sm] made a label of zero to 100

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import FlexGrid from './components/FlexGrid/FlexGrid';
 
 function App() {
   return (
-    <div className="labels">
+    <div className="label-wrapper">
+      <div className="x-zero label">0</div>
+      <div className="y-zero label">0</div>
+      <div className="x-100 label">100</div>
+      <div className="y-100 label">100</div>
       <FlexGrid />
     </div>
   );
