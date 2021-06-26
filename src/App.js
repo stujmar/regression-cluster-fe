14e18012ada39895ce7208a8c61d7f517cb5316e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-26 09:59:07 -0500

    [sm] made the brid a giant button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <div className="y-zero label">0</div>
       <div className="x-100 label">100</div>
       <div className="y-100 label">100</div>
-      <FlexGrid />
+      <button className="clean-button" onClick={(() => {console.log("grid clicked")})}>
+        <FlexGrid />
+      </button>
     </div>
   );
 }
