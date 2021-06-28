673b75096cd9b6592550e2a519ca34ee2db22c2b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-27 21:15:46 -0500

    [sm] wow set up rerender counter as a ref and now it doesn't trigger an infinite loop of rerendering

--- src/components/MyRef.js
+++ src/components/MyRef.js
@@ -2,17 +2,19 @@ import React, { useEffect, useState, useRef } from 'react';
 
 const MyRef = () => {
     const [name, setName] = useState('Stu');
-    const [renderCount, setRenderCount] = useState(0);
+    const renderCount = useRef(0);
 
     useEffect(() => {
-        setRenderCount(prevRenderCount => prevRenderCount + 1);
+        renderCount.current = renderCount.current + 1;
     })
+
     return (
-        <>
-            <input value={name} onChange={e => setName(e.target.value)}/>
-            <div>My name is {name}</div>
-            count:{renderCount}
-        </>)
+            <div>
+                <input value={name} onChange={e => setName(e.target.value)}/>
+                <div>My name is {name}</div>
+                count:{renderCount.current}
+            </div>
+        )
 }
 
 export default MyRef
